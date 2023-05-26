ee9568a3a7f3d2ca43307a4bf383cf3a88ba33fb
josealfredomatias493 <80073145+josealfredomatias493@users.noreply.github.com>
2023-05-26 19:04:08 -0400

    Create index_view.py

---
+++ index_view.py
@@ -0,0 +1,18 @@
+import tkinter as tk
+from tkinter import ttk
+from tkinter.font import families
+
+
+class main_bar():
+    def __init__(self, _frameLogo, _frameUser):
+        self._frameLogo = _frameLogo
+        self._frameUser = _frameUser
+
+    def main_bar_view(self):
+        # Creating title megamercado
+        self.main_title_reg_pro = tk.Label(self._frameLogo, text='MEGAMERCADO', font=('Roboto Mono Bold', 15), bg='#37648B', fg='white')
+        self.main_title_reg_pro.grid()
+        
+
+        self.user_log = tk.Label(self._frameUser, text='Usuario ADM', font=('Roboto Mono Bold', 15), bg='#37648B', fg='white')
+        self.user_log.grid()
